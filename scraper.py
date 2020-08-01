43794dc509c23f7ae4f5ea16e9dafefe2e0efee5
rabah babaci <4rabah@gmail.com>
2020-08-01 13:26:07 -0700

    schedule the tracker with crontab to run daily

--- scraper.py
+++ scraper.py
@@ -2,11 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 import smtplib
 import time
+from datetime import date
 
 URL = "https://www.bestbuy.com/site/dji-mavic-2-pro-quadcopter-with-remote-controller-gray/6262620.p?skuId=6262620"
 
 headers = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36"}
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36"
+}
 
 
 def check_price():
@@ -19,8 +21,9 @@ def check_price():
 
     if converted_price <= 1500:
         send_email()
-
-    print(converted_price)
+    else:
+        today = date.today()
+        print(f"{today}: price: {converted_price}.")
 
 
 def send_email():
@@ -31,16 +34,12 @@ def send_email():
 
     server.login("4rabah@gmail.com", "nvvtlxvpglhzyumz")
 
-    subject = 'Best Buy DJI Mavic Pro price is down!!!'
+    subject = "Best Buy DJI Mavic Pro price is down!!!"
     body = "Check the Best Buy Link https://www.bestbuy.com/site/dji-mavic-2-pro-quadcopter-with-remote-controller-gray/6262620.p?skuId=6262620"
 
     message = f"Subject: {subject}\n\n{body}"
 
-    server.sendmail(
-        "4rabah@gmail.com",
-        "rbabaci1@gmail.com",
-        message
-    )
+    server.sendmail("4rabah@gmail.com", "rbabaci1@gmail.com", message)
     print("Hey, Email has been sent!")
 
     server.quit()
