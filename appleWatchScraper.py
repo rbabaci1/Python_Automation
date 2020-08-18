3c61e1f04b684e51f87ffc70679f089bb073595f
rabah babaci <4rabah@gmail.com>
2020-08-18 15:44:19 -0700

    create a tracker for apple watck series 5

--- appleWatchScraper.py
+++ appleWatchScraper.py
@@ -4,7 +4,7 @@ import smtplib
 import time
 from datetime import date
 
-URL = "https://www.bestbuy.com/site/dji-mavic-2-pro-quadcopter-with-remote-controller-gray/6262620.p?skuId=6262620"
+URL = "https://www.bestbuy.com/site/apple-watch-nike-series-5-gps-cellular-44mm-space-gray-aluminum-case-with-anthracite-black-nike-sport-band-space-gray-aluminum/6215967.p?skuId=6215967"
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36"
@@ -19,7 +19,7 @@ def check_price():
     price = soup.find(class_="priceView-customer-price").get_text()
     converted_price = float(price[1:6].replace(",", ""))
 
-    if converted_price <= 1500:
+    if converted_price <= 500:
         send_email()
     else:
         print(f"{date.today()} - price: ${converted_price}")
@@ -31,8 +31,8 @@ def send_email():
     server.starttls()
     server.ehlo()
 
-    subject = "Best Buy DJI Mavic Pro price is down!!!"
-    body = "Check the Best Buy Link https://www.bestbuy.com/site/dji-mavic-2-pro-quadcopter-with-remote-controller-gray/6262620.p?skuId=6262620"
+    subject = "Apple Watch Nike Series 5 price is down!!!"
+    body = "Check the Best Buy Link:  https://www.bestbuy.com/site/apple-watch-nike-series-5-gps-cellular-44mm-space-gray-aluminum-case-with-anthracite-black-nike-sport-band-space-gray-aluminum/6215967.p?skuId=6215967"
 
     message = f"Subject: {subject}\n\n{body}"
 
